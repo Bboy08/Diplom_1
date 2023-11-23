098893a074614b58912044674dea2e01f10a233d
Anton Bochaev <anton-bochaev@yandex.ru>
2023-11-23 14:45:26 +0400

    Diplom_1

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,24 +1,47 @@
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import praktikum.Bun;
-
 import static junit.framework.TestCase.assertEquals;
 
+@RunWith(Parameterized.class)
 public class BunTest {
+    private String bunName;
+    private float bunPrice;
 
-    @Test
-    public void getNameReturnRightName() {
-        Bun bun = new Bun("Булочка c кунжутом", 27.5F);
-        String actualResult = bun.getName();
-        String expectedResult = "Булочка c кунжутом";
-        assertEquals(expectedResult, actualResult);
+    public BunTest(String bunName, float bunPrice) {
+        this.bunName = bunName;
+        this.bunPrice = bunPrice;
+    }
+
+    @Parameterized.Parameters()
+    public static Object[][] getTestData() {
+        return new Object[][]{
+                {"black bun", 100f},
+                {"white bun", 200f},
+                {"red bun", 300f},
+                {null, 100f},
+                {"", 200f},
+                {"black bun Very Long Bun Name", 3.5f},
+                {"Special @ Bun", 4.0f},
+                {"red bun", -1.0f},
+                {"white bun", 0.0f},
+                {"black bun", -Float.MAX_VALUE},
+                {"white bun", Float.MAX_VALUE},
+                {"red bun", Float.MIN_VALUE},
+        };
     }
 
     @Test
-    public void getPriceReturnRightPrice() {
-        Bun bun = new Bun("Булочка c кунжутом", 27.5F);
-        float actualResult = bun.getPrice();
-        float expectedResult = 27.5F;
-        assertEquals(expectedResult, actualResult);
+    public void checkGetValidName() {
+        Bun bun = new Bun(bunName, bunPrice);
+        assertEquals("invalid name", bunName, bun.getName());
+    }
+
+    @Test
+    public void checkGetValidPrice() {
+        Bun bun = new Bun(bunName, bunPrice);
+        assertEquals("invalid price", bunPrice, bun.getPrice(), 0.001);
     }
 
 }
--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -1,72 +1,83 @@
-import org.hamcrest.MatcherAssert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.*;
-import java.util.List;
 import static junit.framework.TestCase.assertEquals;
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.core.AllOf.allOf;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BurgerTest {
+    @InjectMocks
+    private Burger burger;
+
     @Mock
-    List<Ingredient> ingredients;
+    private Bun mockBun;
 
-    @Test
-    public void addIngredientMethodAddCalled(){
-        Burger burger = new Burger();
-        burger.addIngredient(ingredients.get(1));
-        Mockito.verify(ingredients).add(ingredients.get(1));
+    @Mock
+    private Ingredient mockIngredient;
+
+    private final IngredientType sauceType = IngredientType.SAUCE;
+    private final String sauceName = "Mayonnaise";
+    private final float saucePrice = 100F;
+    private final String bunName = "Bread";
+    private final float bunPrice = 50F;
+
+    @Before
+    public void setUp() {
+        burger = new Burger();
+        burger.setBuns(mockBun);
     }
 
+    @Test
+    public void testAddIngredient() {
+        burger.addIngredient(mockIngredient);
+        assertTrue(burger.ingredients.contains(mockIngredient));
+    }
 
     @Test
-    public void removeIngredientMethodRemoveCalled(){
-        Burger burger = new Burger();
-        burger.addIngredient(ingredients.get(0));
+    public void testRemoveIngredient() {
+        burger.addIngredient(mockIngredient);
         burger.removeIngredient(0);
-        Mockito.verify(ingredients).remove(ingredients.get(0));
+        assertTrue(burger.ingredients.isEmpty());
     }
 
     @Test
-    public void moveIngredientMethodRemoveCalled(){
-        Burger burger = new Burger();
-        burger.addIngredient(ingredients.get(0));
-        burger.addIngredient(ingredients.get(1));
+    public void testMoveIngredient() {
+        Ingredient mockIngredient2 = mock(Ingredient.class);
+        burger.addIngredient(mockIngredient);
+        burger.addIngredient(mockIngredient2);
         burger.moveIngredient(0, 1);
-        Mockito.verify(ingredients).remove(ingredients.get(0));
-    }
-
-
-    @Test
-    public void getPriceRightPrice(){
-        Burger burger = new Burger();
-        Database database = new Database();
-        List<Bun> buns = database.availableBuns();
-        List<Ingredient> ingredients = database.availableIngredients();
-        burger.setBuns(buns.get(1));
-        burger.addIngredient(ingredients.get(0));
-        float actualResult = burger.getPrice();
-        float expectedResult = 500.0F;
-        assertEquals(expectedResult, actualResult);
+        assertEquals(mockIngredient2, burger.ingredients.get(0));
+        assertEquals(mockIngredient, burger.ingredients.get(1));
     }
 
     @Test
-    public void getReceiptRightRecipe(){
-        Database database = new Database();
-        List<Bun> buns = database.availableBuns();
-        List<Ingredient> ingredients = database.availableIngredients();
-        Burger burger = new Burger();
-        burger.setBuns(buns.get(1));
-        burger.addIngredient(ingredients.get(0));
-        String actualResult = burger.getReceipt();
-        String expectedWhiteBun = "white bun";
-        String expectedSauceHotSauce = "sauce hot sauce";
-        String expectedPrice = "Price: 500,000000";
-        MatcherAssert.assertThat(actualResult, allOf(containsString(expectedWhiteBun), containsString(expectedSauceHotSauce), containsString(expectedPrice)));
+    public void testGetPrice() {
+        when(mockBun.getPrice()).thenReturn(100f);
+        when(mockIngredient.getPrice()).thenReturn(100f);
+        burger.addIngredient(mockIngredient);
+        assertEquals(300f, burger.getPrice(), 0.01);
+    }
+
+    @Test
+    public void getReceiptTest() {
+        float totalPrice = (bunPrice * 2) + saucePrice;
+        String receiptER = String.format("(==== %s ====)%n= %s %s =%n(==== %s ====)%n%nPrice: %f%n",
+                bunName, sauceType.toString().toLowerCase(), sauceName, bunName, totalPrice);
+        burger.setBuns(mockBun);
+        burger.addIngredient(mockIngredient);
+        Mockito.when(mockBun.getName()).thenReturn(bunName);
+        Mockito.when(mockBun.getPrice()).thenReturn(bunPrice);
+        Mockito.when(mockIngredient.getType()).thenReturn(sauceType);
+        Mockito.when(mockIngredient.getName()).thenReturn(sauceName);
+        Mockito.when(mockIngredient.getPrice()).thenReturn(saucePrice);
+        assertEquals(receiptER, burger.getReceipt());
     }
 
 }
